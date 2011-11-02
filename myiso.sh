4985937b69f832448fd392ef7127f64abc3e8f6f
Marc Villacorta <marc.villacorta@gmail.com>
2011-11-02 23:38:09 +0100

    Get puppet from puppetlabs repository.

--- myiso.sh
+++ myiso.sh
@@ -4,7 +4,18 @@
 # Checking arguments:
 #------------------------------------------------------------------------------
 
-if [ $# -ne 1 ]; then echo "Usage: `basename $0` [1|2|3|4|5|6]"; exit; fi
+if [ $# -ne 1 ]; then
+
+    echo "Usage: `basename $0` [1|2|3|4|5]"
+    echo
+    echo " 1: Download, mount and rsync the ISO without the RPMs."
+    echo " 2: Download the latest version of puppet and the core group of RPMs."
+    echo " 3: Generate the repository metadata."
+    echo " 4: Inject the kickstart.cfg file into the initrd.img bundle."
+    echo " 5: Build the final ISO file."
+    echo
+    exit
+fi
 
 set -e
 
@@ -22,76 +33,109 @@ pkg="${tmp}/Packages"
 dat="${tmp}/repodata"
 isl="${tmp}/isolinux"
 
-packages="ruby libselinux-ruby augeas-libs"
+#------------------------------------------------------------------------------
+# yum.conf
+#------------------------------------------------------------------------------
+
+yumconf="[main]\n\
+cachedir=/var/cache/yum/\$basearch/\$releasever\n\
+keepcache=0\n\
+debuglevel=2\n\
+logfile=/var/log/yum.log\n\
+exactarch=1\n\
+obsoletes=1\n\
+gpgcheck=1\n\
+plugins=1\n\
+installonly_limit=5\n\
+bugtracker_url=http://bugs.centos.org/set_project.php?project_id=16&ref=http://bugs.centos.org/bug_report_page.php?category=yum\n\
+distroverpkg=centos-release\n\
+exclude=*.i?86\n\
+\n\
+[puppet1]\n\
+name=Puppetlabs Packages - \$basearch\n\
+baseurl=http://yum.puppetlabs.com/el/6/products/\$basearch\n\
+enabled=1\n\
+gpgcheck=1\n\
+gpgkey=http://yum.puppetlabs.com/RPM-GPG-KEY-puppetlabs\n\
+\n\
+[puppet2]\n\
+name=Puppetlabs Dependencies - \$basearch\n\
+baseurl=http://yum.puppetlabs.com/el/6/dependencies/\$basearch\n\
+enabled=1\n\
+gpgcheck=1\n\
+gpgkey=http://yum.puppetlabs.com/RPM-GPG-KEY-puppetlabs"
 
 #------------------------------------------------------------------------------
 # puppet.conf
 #------------------------------------------------------------------------------
 
-puppet="[main]\n
-\n
-    confdir    = /etc/puppet\n
-    vardir     = /var/lib/puppet\n
-    logdir     = /var/log/puppet\n
-    rundir     = /var/run/puppet\n
-    ssldir     = \\\$vardir/ssl\n
-    pluginsync = true\n
-\n
-[agent]\n
-\n
-    classfile     = \\\$vardir/classes.txt\n
-    localconfig   = \\\$vardir/localconfig\n
-    graphdir      = \\\$vardir/state/graphs\n
-    graph         = true\n
-    factsignore   = .svn CVS .git *.markdown .*.swp\n
-    pluginsignore = .svn CVS .git *.markdown .*.swp\n
-\n
-[master]\n
-\n
-    modulepath = \\\$confdir/modules:\\\$confdir/roles\n"
+puppet="[main]\n\
+\n\
+    confdir    = /etc/puppet\n\
+    vardir     = /var/lib/puppet\n\
+    logdir     = /var/log/puppet\n\
+    rundir     = /var/run/puppet\n\
+    ssldir     = \\\$vardir/ssl\n\
+    pluginsync = true\n\
+\n\
+[agent]\n\
+\n\
+    classfile     = \\\$vardir/classes.txt\n\
+    localconfig   = \\\$vardir/localconfig\n\
+    graphdir      = \\\$vardir/state/graphs\n\
+    graph         = true\n\
+    factsignore   = .svn CVS .git *.markdown .*.swp\n\
+    pluginsignore = .svn CVS .git *.markdown .*.swp\n\
+\n\
+[master]\n\
+\n\
+    modulepath = \\\$confdir/modules:\\\$confdir/roles\n\
+"
 
 #------------------------------------------------------------------------------
 # kickstart.cfg
 #------------------------------------------------------------------------------
 
-kickstart="install\n
-text\n
-cdrom\n
-lang en_US.UTF-8\n
-keyboard es\n
-network --device eth0 --bootproto dhcp\n
-rootpw  password\n
-firewall --disabled\n
-authconfig --enableshadow --passalgo=sha512 --enablefingerprint\n
-selinux --disabled\n
-services --disabled auditd,fcoe,ip6tables,iptables,iscsi,iscsid,lldpad,netfs,nfslock,rpcbind,rpcgssd,rpcidmapd,udev-post,lvm2-monitor\n
-timezone --utc Europe/Madrid\n
-bootloader --location=mbr --driveorder=sda --append=\"crashkernel=auto rhgb quiet\"\n
-clearpart --all --drives=sda\n
-ignoredisk --only-use=sda\n
-part /boot --fstype=ext4 --size 200\n
-part pv.0 --grow --size=1\n
-volgroup vg0 --pesize=4096 pv.0\n
-logvol / --fstype=ext4 --name=lv0 --vgname=vg0 --size=2048 --grow --maxsize=5120\n
-%packages --nobase\n
-puppet\n
-%post\n
-sed -i 's/timeout=./timeout=0/' /boot/grub/grub.conf\n
-sed -i 's/ rhgb//' /boot/grub/grub.conf\n
-echo \"91.121.159.192 puppet\" >> /etc/hosts\n
-echo -e \"${puppet}\" > /etc/puppet/puppet.conf\n
-%end\n"
+kickstart="install\n\
+text\n\
+cdrom\n\
+lang en_US.UTF-8\n\
+keyboard es\n\
+network --device eth0 --bootproto dhcp\n\
+rootpw  password\n\
+firewall --disabled\n\
+authconfig --enableshadow --passalgo=sha512 --enablefingerprint\n\
+selinux --disabled\n\
+services --disabled auditd,fcoe,ip6tables,iptables,iscsi,iscsid,lldpad,netfs,nfslock,rpcbind,rpcgssd,rpcidmapd,udev-post,lvm2-monitor\n\
+timezone --utc Europe/Madrid\n\
+bootloader --location=mbr --driveorder=sda --append=\"crashkernel=auto rhgb quiet\"\n\
+clearpart --all --drives=sda\n\
+ignoredisk --only-use=sda\n\
+part /boot --fstype=ext4 --size 200\n\
+part pv.0 --grow --size=1\n\
+volgroup vg0 --pesize=4096 pv.0\n\
+logvol / --fstype=ext4 --name=lv0 --vgname=vg0 --size=2048 --grow --maxsize=5120\n\
+%packages --nobase\n\
+puppet\n\
+%post\n\
+sed -i 's/timeout=./timeout=0/' /boot/grub/grub.conf\n\
+sed -i 's/ rhgb//' /boot/grub/grub.conf\n\
+echo \"91.121.159.192 puppet\" >> /etc/hosts\n\
+echo -e \"${puppet}\" > /etc/puppet/puppet.conf\n\
+%end\n\
+"
 
 #------------------------------------------------------------------------------
 # isolinux.cfg
 #------------------------------------------------------------------------------
 
-isolinux="default kickstart\n
-\n
-label kickstart\n
-  menu label Kickstart\n
-  kernel vmlinuz\n
-  append ks=file:/kickstart.cfg initrd=initrd.img\n"
+isolinux="default kickstart\n\
+\n\
+label kickstart\n\
+  menu label Kickstart\n\
+  kernel vmlinuz\n\
+  append ks=file:/kickstart.cfg initrd=initrd.img\n\
+"
 
 #------------------------------------------------------------------------------
 #
@@ -100,7 +144,7 @@ label kickstart\n
 case "$1" in
 
     #-------------------------------------------------
-    # Retrieve, mount and rsync the ISO w/o the RPMs:
+    # Download, mount and rsync the ISO w/o the RPMs:
     #-------------------------------------------------
 
     1) test -f ${iso} || wget ${url}
@@ -109,39 +153,28 @@ case "$1" in
        rsync -a --delete --exclude="*.rpm" --exclude="*.TBL" --delete-excluded ${mnt}/ ${tmp}/
        ;;
 
-    #------------------------------------------
-    # Retrieve the updated core group of RPMs:
-    #------------------------------------------
+    #-------------------------------------------------------------------
+    # Download the latest version of puppet and the core group of RPMs:
+    #-------------------------------------------------------------------
 
-    2) test -f /tmp/myiso.conf || cp /etc/yum.conf /tmp/myiso.conf
-       if [ ${arch} == 'x86_64' ]; then grep -q "^exclude=\*\.i?86$" /tmp/myiso.conf || echo "exclude=*.i?86" >> /tmp/myiso.conf; fi
-       grep "mandatory" ${dat}/*minimal*.xml | awk -F '[<|>]' '{print $3}' | xargs repotrack -c /tmp/myiso.conf -p ${pkg} ${packages}
+    2) echo -e ${yumconf} > /tmp/yum.conf
+       grep "mandatory" ${dat}/*minimal*.xml | awk -F '[<|>]' '{print $3}' | xargs repotrack -c /tmp/yum.conf -p ${pkg} puppet
        ;;
 
-    #------------------------------
-    # Retrieve puppet client RPMs:
-    #------------------------------
+    #-----------------------------------
+    # Generate the repository metadata:
+    #-----------------------------------
 
-    3) test -f ${pkg}/ruby-augeas-0.3.0-1.el6.${arch}.rpm || wget http://yum.puppetlabs.com/el/6/dependencies/${arch}/ruby-augeas-0.3.0-1.el6.${arch}.rpm -P ${pkg}
-       test -f ${pkg}/ruby-shadow-1.4.1-13.el6.${arch}.rpm || wget http://yum.puppetlabs.com/el/6/dependencies/${arch}/ruby-shadow-1.4.1-13.el6.${arch}.rpm -P ${pkg}
-       test -f ${pkg}/facter-1.6.1-1.el6.noarch.rpm || wget http://yum.puppetlabs.com/el/6/products/${arch}/facter-1.6.1-1.el6.noarch.rpm -P ${pkg}
-       test -f ${pkg}/puppet-2.7.5-1.el6.noarch.rpm || wget http://yum.puppetlabs.com/el/6/products/${arch}/puppet-2.7.5-1.el6.noarch.rpm -P ${pkg}
-       ;;
-
-    #---------------------------------
-    # Create the repository metadata:
-    #---------------------------------
-
-    4) mv ${dat}/*minimal*.xml ${dat}/minimal.xml
+    3) mv ${dat}/*minimal*.xml ${dat}/minimal.xml
        find ${dat}/* ! -name minimal.xml -delete
        createrepo --database --unique-md-filenames -g ${dat}/minimal.xml -o ${tmp} ${tmp}
        ;;
 
-    #-----------------------
-    # Inject kickstart.cfg:
-    #-----------------------
+    #-----------------------------------------------------------
+    # Inject the kickstart.cfg file into the initrd.img bundle:
+    #-----------------------------------------------------------
 
-    5) cd ${isl}
+    4) cd ${isl}
        gunzip initrd.img --suffix .img
        mkdir cpio-initrd; cd cpio-initrd
        cpio -id < ../initrd
@@ -152,11 +185,11 @@ case "$1" in
        echo -e ${isolinux} > isolinux.cfg
        ;;
 
-    #----------------------
-    # Build the final ISO:
-    #----------------------
+    #---------------------------
+    # Build the final ISO file:
+    #---------------------------
 
-    6) mkisofs -l -J -R -r -T -o /media/sf_shared/boot.iso -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table ${tmp}
+    5) mkisofs -l -J -R -r -T -o /media/sf_shared/boot.iso -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table ${tmp}
        sync
        ;;
 esac
